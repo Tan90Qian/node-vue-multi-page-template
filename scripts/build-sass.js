d3112a0a2b05d30a48f7f8917ae1c0f83270e83e
qianjiacheng <jiacheng.qian@manaowan.com>
2018-10-29 15:44:19 +0800

    按功能转移代码

--- scripts/build-sass.js
+++ scripts/build-sass.js
@@ -1,4 +1,60 @@
-import { sasscDev, sassc } from './webpack-config/config-producer';
+import fs2 from 'fs-extra';
+import process from 'child_process';
+
+import { transformEntries, collectFiles } from './webpack-config/config-producer';
+
+const chokidar = require('chokidar');
+
+/* 编译scss文件 */
+function sassc() {
+  fs2.removeSync(webpackPath.cssPath);
+  let scssEntry = collectFiles(webpackPath.views, webpackPath.scssEntry, []);
+  scssEntry = transformEntries(scssEntry);
+  Object.keys(scssEntry).forEach(entry => {
+    const filePath = scssEntry[entry];
+    process.execSync(
+      `node-sass ${filePath} static${path.sep}css${path.sep}${entry}.css`, 
+      function (error) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+      }
+    )
+  })
+  console.log('finish build scss');
+}
+
+/* 根据watch编译单个scss文件 */
+function sasscSimple(filePath) {
+  const filePathArray = filePath.split(path.sep);
+  const [targetName] = filePathArray.slice(-2);
+  process.execSync(
+    `node-sass ${filePath} static${path.sep}css${path.sep}${targetName}.css`, 
+    function (error) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    }
+  )
+  console.log(`update finish file: ${targetName}`);
+}
+
+/* 监听scss文件变化并编译 */
+function sasscDev() {
+  fs2.removeSync(webpackPath.cssPath);
+  const watcher = chokidar.watch('./views/**/art-style.scss')
+  watcher.on('all', (event, filePath) => {
+    if (event !== 'error') {
+      sasscSimple(filePath);
+    } else {
+      const error = filePath;
+      console.error(error);
+      watcher.close();
+    }
+  })
+  console.log('start watch!');
+}
+
 
 const args = process.argv.filter(arg => arg.slice(0,4) === 'env=');
 let env = 'production';
--- scripts/webpack-config/config-producer.js
+++ scripts/webpack-config/config-producer.js
@@ -1,12 +1,8 @@
 import path from 'path';
 import fs from 'fs';
-import fs2 from 'fs-extra';
-import process from 'child_process';
 
 import webpackPath from './path';
 import baseConfig from './base-config';
-
-const chokidar = require('chokidar');
 
 /**
  * collect all vue-entry.js in views folder, DFS algorithm
@@ -14,7 +10,7 @@ const chokidar = require('chokidar');
  * @param {string} targetFileName name of target files
  * @param {string[]} files file names (pages/home/vue-entry.js -> pages/home)
  */
-function collectFiles(targetFolder, targetFileName, files) {
+export function collectFiles(targetFolder, targetFileName, files) {
   const folders = [];
   fs.readdirSync(targetFolder)
   .map(name => path.join(targetFolder, name))
@@ -32,7 +28,7 @@ function collectFiles(targetFolder, targetFileName, files) {
  * @return {object} { fileName: path } 
  * @return {string} fileName: relative path to pages. such as pages/home-bundle
  */
-function transformEntries(entries, postfix = '') {
+export function transformEntries(entries, postfix = '') {
   const output = {};
   entries.forEach(entry => {
     const segments = entry.split(path.sep);
@@ -42,56 +38,6 @@ function transformEntries(entries, postfix = '') {
     output[fileName] = `.${path.sep}${entry}`;
   })
   return output;
-}
-
-/* 编译scss文件 */
-export function sassc() {
-  fs2.removeSync(webpackPath.cssPath);
-  let scssEntry =  collectFiles(webpackPath.views, webpackPath.scssEntry, []);
-  scssEntry = transformEntries(scssEntry);
-  Object.keys(scssEntry).forEach(entry => {
-    const filePath = scssEntry[entry];
-    process.execSync(
-      `node-sass ${filePath} static${path.sep}css${path.sep}${entry}.css`, 
-      function (error) {
-        if (error !== null) {
-          console.log('exec error: ' + error);
-        }
-      }
-    )
-  })
-  console.log('finish build scss');
-}
-
-/* 根据watch编译单个scss文件 */
-function sasscSimple(filePath) {
-  const filePathArray = filePath.split(path.sep);
-  const [targetName] = filePathArray.slice(-2);
-  process.execSync(
-    `node-sass ${filePath} static${path.sep}css${path.sep}${targetName}.css`, 
-    function (error) {
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-    }
-  )
-  console.log(`update finish file: ${targetName}`);
-}
-
-/* 监听scss文件变化并编译 */
-export function sasscDev() {
-  fs2.removeSync(webpackPath.cssPath);
-  const watcher = chokidar.watch('./views/**/art-style.scss')
-  watcher.on('all', (event, filePath) => {
-    if (event !== 'error') {
-      sasscSimple(filePath);
-    } else {
-      const error = filePath;
-      console.error(error);
-      watcher.close();
-    }
-  })
-  console.log('start watch!');
 }
 
 /**
