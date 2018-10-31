58864dbb0a855f694d3fd9b377289a7e382a7dad
qianjiacheng <jiacheng.qian@manaowan.com>
2018-10-31 20:51:11 +0800

    lint ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,6 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['airbnb-base', 'plugin:vue/essential', 'prettier'],
+  extends: ['plugin:vue/essential', 'prettier'],
   plugins: ['vue'],
-  rules: {
-    'import/extensions': [0],
-    'import/no-extraneous-dependencies': [0],
-    'import/no-unresolved': [0],
-    'no-new': [0],
-    'no-console': [0],
-  },
 };
