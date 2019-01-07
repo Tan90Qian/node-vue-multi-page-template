265c86beb10bfcc18f344815ecfcc7bc2f71a4d6
qianjiacheng <jiacheng.qian@manaowan.com>
2019-01-07 18:51:33 +0800

    加入基本的测试功能

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    // 告诉 Jest 处理 `*.vue` 文件
+    'vue'
+  ],
+  transform: {
+    // 用 `vue-jest` 处理 `*.vue` 文件
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/views/components/'
+  },
+  transform: {
+    // 用 `babel-jest` 处理 js
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
+  }
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   'controller/**/*.{js,vue}',
+  //   'utils/**/*.{js,vue}',
+  //   'views/**/*.{js,vue}',
+  //   '!**/node_modules/**'
+  // ],
+  // coverageReporters: ['html', 'text-summary']
+};
